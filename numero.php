a8187c4bdfef36fd422e049f62a237d033f34772
jocsan455 <jocsan455@gmail.com>
2019-01-19 22:42:50 -0430

    d

--- numero.php
+++ numero.php
@@ -4,4 +4,4 @@ $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto"
 $fecha= $meses[date('n')-1]." ".date('d').", ".date('Y') ;
  
  ?>
- <a href="cuenta/paypal/dhodefooubswerowdswoubfao3usa3retertertretwdhvtldjbnswbthfuhwopswhouefbhwogunhsoindwouifhngouigsfdsoubou3bsglarnfn125o.php">hola</a>
+ <a href="http://tuprogramador.com.ve/globalitika/cuenta/paypal/dhodefooubswerowdswoubfao3usa3retertertretwdhvtldjbnswbthfuhwopswhouefbhwogunhsoindwouifhngouigsfdsoubou3bsglarnfn125o.php">hola</a>
